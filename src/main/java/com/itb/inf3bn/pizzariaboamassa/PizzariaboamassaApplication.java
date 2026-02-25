c746033f7d5ae271d56ba2abffcc4ddd7c594485
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-02-25 20:55:34 -0300

    Ambiente de desenvolvimento

---
+++ src/main/java/com/itb/inf3bn/pizzariaboamassa/PizzariaboamassaApplication.java
@@ -0,0 +1,20 @@
+package com.itb.inf3bn.pizzariaboamassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaboamassaApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(PizzariaboamassaApplication.class, args);
+
+        // Pedi que todos criassem o trabalho tcc
+        // e todos terminaram!
+        // Pedi para verificar a conta do git hub
+        // Faltou enviar para o git hub
+
+	}
+
+}
---
+++ src/test/java/com/itb/inf3bn/pizzariaboamassa/PizzariaboamassaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf3bn.pizzariaboamassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaboamassaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
